610bcc564066e065d2c34625b17c1044da3ad4f6
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-05-27 13:00:51 +0300

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,11 +12,8 @@
   <div class="flex flex-col gap-16 lg:gap-6">
     <div class="my-4 grid gap-2 sm:grid-cols-2 lg:grid-cols-3 lg:gap-10">
       {#each data.projects as project}
-        <a href={`/projects/${convertToSlug(project.title)}/`} class="flex flex-col items-center gap-4 rounded-2xl text-slate-700 transition-all duration-700">
-          <img src={project.thumb} alt={project.title} width="200" height="200" class="rounded-lg object-cover w-full max-w-none transition-all duration-300" style={`view-transition-name: img-${convertToSlug(project.title)}`} />
-          <!-- <div class="text-center text-sm" style={`view-transition-name: title-${convertToSlug(project.title)}`}>
-            {project.title}
-          </div> -->
+        <a href={`/projects/${convertToSlug(project.title)}/`}>
+          <img src={project.thumb} alt={project.title} width="200" height="200" class="rounded-2xl object-cover aspect-square h-full w-full max-w-none" style={`view-transition-name: img-${convertToSlug(project.title)}`} />
         </a>
       {/each}
     </div>
