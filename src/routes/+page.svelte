862a17d70bd0f91947f4efd226c491bb4d276124
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-05-27 15:09:02 +0300

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,9 @@
 {#if data.projects && data.projects.length}
   <div class="flex flex-col gap-16 lg:gap-6">
     <div class="my-4 grid gap-2 sm:grid-cols-2 lg:grid-cols-3 lg:gap-10">
-      {#each data.projects as project}
-        <a href={`/projects/${convertToSlug(project.title)}/`}>
-          <img src={project.thumb} alt={project.title} width="200" height="200" class="rounded-2xl object-cover aspect-square h-full w-full max-w-none" style={`view-transition-name: img-${convertToSlug(project.title)}`} />
+      {#each data.projects as project, index}
+        <a href={`/projects/${convertToSlug(project.title)}/`} class="animate-[appear_ease-out]" style={`animation-duration: ${(index + 0.5) * 0.3}s;`}>
+          <img src={project.thumb} alt={project.title} width="200" height="200" class="rounded-2xl object-cover aspect-square h-full w-full max-w-none" />
         </a>
       {/each}
     </div>
