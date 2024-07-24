ccd626a2c8202f7911220a768f50bcb4d73dd20a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-07-24 16:42:07 +0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
-  import "../app.css"
-  import Sidebar from "$lib/components/Sidebar.svelte"
+import "../app.css";
+import Sidebar from "$lib/components/Sidebar.svelte";
 </script>
 
 <svelte:head>
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700;900&display=swap" rel="stylesheet" />
 
-  <link rel="icon" type="image/x-icon" href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'><text x='0' y='14'>👨‍💻</text></svg>" />
+  <link rel="icon" type="image/x-icon" href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'><text x='0' y='14'>👩🏻‍🎨</text></svg>" />
 </svelte:head>
 
 <div class="pb-24 pt-6 flex flex-col items-center justify-center px-6">
